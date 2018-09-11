6c1b2dc8d5d5ecb62e10234106a0908651b210d0
Adam Spiers <git@adamspiers.org>
2018-09-11 10:37:10 -0600

    update demo script

--- SCRIPT.sh
+++ SCRIPT.sh
@@ -1,136 +1,44 @@
 # Demo script
 #
-# Use http://doitlive.readthedocs.io/en/stable/ ?
-#doitlive shell: /bin/zsh
-
-# setup in window 1
-cd ~/londongit
-bash create-repo.sh </dev/null && ( cd test-repo && gitk --all & )
-
-# setup in window 2
-cd test-repo
-
-################################################################
-# git-splice removal of commits from a branch
-
-# Remove a commit
-git splice file-b-three-bar^!
-
-ggrh @{1}  # reset
-
-# Remove a commit range
-git splice file-a-three-foo..file-a-eight-foo
-
-ggrh @{1}
-
-# Abort removing a commit range with conflicts
-git splice file-b..file-a-eight-foo
-
-git splice --in-progress
-git splice --abort
-git splice --in-progress
-
-# Resolve conflicts when removing a commit range
-git splice file-b..file-a-eight-foo
-ggmt
-git splice --continue
-
-################################################################
-# git-splice insertion of commits into a branch
-
-# so that's for removing, not very exciting because rebase -i
-# can already do that.
+# Optionally use http://doitlive.readthedocs.io/en/stable/
+# to make the demo more bulletproof.  Run via:
 #
-# But it can also splice commits into a branch as well as out of it.
-# So let's add a feature branch
+#   doitlive play SCRIPT.sh
 
-ggrh @{1}
-bash ../create-repo.sh feature </dev/null
+#doitlive shell: /bin/bash
+#doitlive prompt: smiley
+#doitlive commentecho: false
 
-# Splice commit onto branch tip (same as cherry-pick)
-git splice master file-c^!
+cd ~/OpenStack/git/cinder
 
-ggrh @{1}
-
-# Splice range onto branch tip (same as cherry-pick)
-git splice master file-c^..feature^
-
-ggrh @{1}
-
-# Splice commit *into* branch
-git splice master^ file-c^!
-
-ggrh @{1}
-
-# Splice a range *into* branch
-git splice master^ file-c^..feature^
-
+git tag to-backport 46b8da35
+git checkout -t origin/stable/pike
 ################################################################
-# git-splice removal and insertion of commits from/into a branch
-
-ggrh @{1}
-
-git splice file-a-three-foo..file-a-eight-foo file-c^..feature^
-
-################################################################
-# git-transplant
-
-ggrh @{1}
-
-# transplant commit onto tip
-git transplant file-a-eight-foo^! feature
-
-ggrh @{1} && git branch -f feature feature@{1}
-
-# transplant range onto tip
-git transplant file-b-three-bar..master feature
-
-ggrh @{1} && git branch -f feature feature@{1}
-
-# transplant range inside branch
-git transplant --after feature^ file-b-three-bar..master feature
-
-ggrh @{1} && git branch -f feature feature@{1}
-
-# transplant range onto new branch
-git transplant --new-from=feature file-b-three-bar..master feature2
-
-ggrh @{1} && git branch -D feature2
-
-# transplant range inside new branch
-git transplant --new-from=feature^^ file-b-three-bar..master feature2
-
-ggrh @{1} && git branch -D feature2
-
-################################################################
-## git-deps basic demo
-
-git deps master^!
-git deps -l master^!
-git deps -l master^^!
-git deps -l master^^^!
-
-git deps -s
-# Try:
-# master
-# master^
-# master^^
+## Try the naive approach
+git cherry-pick to-backport
+git cherry-pick --abort
 
 ################################################################
 ## git-deps backporting demo
 
-cd ~/londongit
-bash create-repo.sh port </dev/null && ( cd test-repo && gitk --all & )
-cd test-repo
+git deps -h
+git deps -e origin/stable/pike to-backport^!
+git deps -e origin/stable/pike -l to-backport^!
 
-git deps -e `git merge-base master stable` master^!
-git checkout stable
-git cherry-pick $sha
-git cherry-pick master^!
+git deps -s -e origin/stable/pike &
+
+git cherry-pick d9af50b131fedfdf960eecff9093dfeadd6763af
+git cherry-pick f36fc239804fb8fbf57d9df0320e2cb6d315ea10
+git cherry-pick to-backport
+
+cat deps.txt
+git reset --hard origin/stable/pike
+tsort deps.txt | tac | xargs -t git cherry-pick
 
 ################################################################
 ## git-explode
 
+git explode -h
 git explode file-b master
 
 # Delete the new topic branches
