59f2a147fc486d5f7a26011cf9aab62f977f8692
Adam Spiers <git@adamspiers.org>
2018-09-11 11:09:15 -0600

    clear the demo screen for clarity where appropriate

--- SCRIPT.sh
+++ SCRIPT.sh
@@ -8,6 +8,7 @@
 #doitlive shell: /bin/bash
 #doitlive prompt: smiley
 #doitlive commentecho: false
+#doitlive alias: c="clear"
 
 cd ~/OpenStack/git/cinder
 
@@ -20,8 +21,10 @@ git cherry-pick --abort
 
 ################################################################
 ## git-deps backporting demo
-
+c
 git deps -h
+c
+
 git deps -e origin/stable/pike to-backport^!
 git deps -e origin/stable/pike -l to-backport^!
 
@@ -31,14 +34,16 @@ git cherry-pick d9af50b131fedfdf960eecff9093dfeadd6763af
 git cherry-pick f36fc239804fb8fbf57d9df0320e2cb6d315ea10
 git cherry-pick to-backport
 
+c
 cat deps.txt
 git reset --hard origin/stable/pike
 tsort deps.txt | tac | xargs -t git cherry-pick
 
 ################################################################
 ## git-explode
-
+c
 git explode -h
+c
 git explode file-b master
 
 # Delete the new topic branches
