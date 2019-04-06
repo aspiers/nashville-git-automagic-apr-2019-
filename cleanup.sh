aef59ee7c7ae4462f2c378f0acfd878e47d59745
Adam Spiers <git@adamspiers.org>
2019-04-05 20:10:12 -0500

    improve demo setup

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+DEMO_DIR=$HOME/nashville-git/
+
 cd ~/OpenStack/git/cinder
 git checkout master
 git branch -D stable/pike || :
 git tag -d to-backport || :
-cd ~/nashville-git/
-#./create-repo.sh </dev/null # && ( cd test-repo && gitk --all & )
+
+mkdir -p $DEMO_DIR
+cd $DEMO_DIR
+ln -sf ~/.GIT/adamspiers.org/nashville-git-automagic-april-2019/bin/create-repo.sh
+
+./create-repo.sh </dev/null # && ( cd test-repo && gitk --all & )
 cd ~/nashville-git/test-repo
 git show-ref | awk '/\/topic/ {print $2}' | xargs -n1 -r git update-ref -d
 pkill -f git-deps
