087ddfb92ba94e3b579926139e44032e8f2bedb1
Adam Spiers <git@adamspiers.org>
2019-04-05 20:39:20 -0500

    add splice/transplant demo scripts

--- demo/scripts/20-splice-transplant.sh
+++ demo/scripts/20-splice-transplant.sh
@@ -8,50 +8,12 @@
 #doitlive commentecho: false
 #doitlive alias: c="clear"
 
-cd ~/OpenStack/git/cinder
-
-git tag to-backport 46b8da35
-git checkout -t origin/stable/pike
-################################################################
-## Try the naive approach
-git cherry-pick to-backport
-git cherry-pick --abort
-
-################################################################
-## git-deps backporting demo
-c
-git deps -h
-c
-
-git deps -e origin/stable/pike to-backport^!
-git deps -l -r -e origin/stable/pike to-backport^!
-
-c
-git deps -s -e origin/stable/pike
-
-git cherry-pick d9af50b131fedfdf960eecff9093dfeadd6763af
-git cherry-pick f36fc239804fb8fbf57d9df0320e2cb6d315ea10
-git cherry-pick to-backport
-
-c
-git deps -r -e origin/stable/pike to-backport^! | tee deps.txt
-git reset --hard origin/stable/pike
-tsort deps.txt | tac | xargs -t git cherry-pick
-
-################################################################
-## git-explode
-c
-git explode -h
-c
-cd ~/nashville-git/test-repo
-gitk --all &
-git explode file-b master
-
-
 ################################################################
 # git-splice removal of commits from a branch
 
-git show-ref | awk '/\/topic/ {print $2}' | xargs -n1 -r git update-ref -d
+cd ~/nashville-git
+./create-repo.sh </dev/null
+cd test-repo
 
 # Remove a commit
 git splice file-b-three-bar^!
